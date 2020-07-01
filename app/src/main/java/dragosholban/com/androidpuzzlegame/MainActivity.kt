8a5db911889de04e64ca00f590cf8b4d3f0b5382
Juan Ramos <juancagb.17@gmail.com>
2020-07-01 16:35:10 -0500

    Fix camera and external storage permissions

--- app/src/main/java/dragosholban/com/androidpuzzlegame/MainActivity.kt
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.GridView
@@ -15,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import dragosholban.com.androidpuzzlegame.util.checkAndRequestPermission
 import dragosholban.com.androidpuzzlegame.util.toast
 import java.io.File
 import java.io.IOException
@@ -22,6 +24,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     var mCurrentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,49 +46,86 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onImageFromCameraClick(view: View?) {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (intent.resolveActivity(packageManager) != null) {
-            var photoFile: File? = null
-            try {
-                photoFile = createImageFile()
-            } catch (e: IOException) {
-                toast(e.message ?: "", Toast.LENGTH_LONG)
-            }
-            if (photoFile != null) {
-                val photoUri = FileProvider.getUriForFile(this, applicationContext.packageName + ".fileprovider", photoFile)
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
-            }
+    fun startCameraIfPermissionIsGranted(view: View?) {
+        checkAndRequestPermission(
+                getString(R.string.dialog_camera_title),
+                getString(R.string.dialog_camera_explanation),
+                Manifest.permission.CAMERA, Companion.REQUEST_PERMISSION_CAMERA
+        ) {
+            startCameraLogic()
         }
     }
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File? {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // permission not granted, initiate request
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE)
-        } else {
-            // Create an image file name
-            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
-            val imageFileName = "JPEG_" + timeStamp + "_"
-            val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-            val image = File.createTempFile(
-                    imageFileName,  /* prefix */
-                    ".jpg",  /* suffix */
-                    storageDir /* directory */
-            )
-            mCurrentPhotoPath = image.absolutePath // save this to use in the intent
-            return image
+    private fun startCameraLogic() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            createImageFileIfPermissionIsGranted()
         }
-        return null
+    }
+
+    private fun createImageFileIfPermissionIsGranted() {
+        checkAndRequestPermission(
+                getString(R.string.dialog_write_external_storage_title),
+                getString(R.string.dialog_write_external_storage_explanation),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE, Companion.REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE
+        ) {
+
+            capturePhoto()
+        }
+    }
+
+    private fun capturePhoto() {
+        // Log.d("MainActivity", "starting capturePhoto() method")
+
+        // Create a File for the item photo
+        val photoFile: File?
+
+        photoFile = try {
+            createDestinationFile()
+        } catch (ex: IOException) {
+            Log.d("MainActivity", "createDestinationFile() fails => ${ex.localizedMessage}")
+            return
+        }
+
+        val photoURI = FileProvider.getUriForFile(this,BuildConfig.APPLICATION_ID + ".provider", photoFile)
+
+        // Continue only if the File was successfully created
+        val captureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        captureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivityForResult(captureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    @Throws(IOException::class)
+    private fun createDestinationFile(): File {
+        // Path for the temporary image and its name
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFileName = getImageFileName()
+
+        val image = File.createTempFile(
+                imageFileName,  // prefix
+                ".$DEFAULT_PHOTO_EXTENSION",  // suffix
+                storageDirectory // directory
+        )
+
+        // Save a the file path
+        mCurrentPhotoPath = image.absolutePath
+        // Log.d("MainActivity", "currentPhotoPath $mCurrentPhotoPath")
+
+        return image
+    }
+
+    private fun getImageFileName(): String {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        return "JPEG_" + timeStamp + "_"
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onImageFromCameraClick(View(this))
+                    startCameraIfPermissionIsGranted(View(this))
                 }
                 return
             }
@@ -94,11 +134,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             val intent = Intent(this, PuzzleActivity::class.java)
             intent.putExtra("mCurrentPhotoPath", mCurrentPhotoPath)
             startActivity(intent)
         }
+
         if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK) {
             val uri = data!!.data
             val intent = Intent(this, PuzzleActivity::class.java)
@@ -118,9 +160,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val REQUEST_PERMISSION_CAMERA = 1
         private const val REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 2
-        private const val REQUEST_IMAGE_CAPTURE = 1
-        const val REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 3
-        const val REQUEST_IMAGE_GALLERY = 4
+        private const val REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 3
+
+        private const val REQUEST_IMAGE_CAPTURE = 11
+        private const val REQUEST_IMAGE_GALLERY = 12
+        private const val DEFAULT_PHOTO_EXTENSION = "jpg"
     }
 }
--- app/src/main/java/dragosholban/com/androidpuzzlegame/util/ContextExtensions.kt
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/util/ContextExtensions.kt
@@ -1,7 +1,26 @@
 package dragosholban.com.androidpuzzlegame.util
 
+import androidx.appcompat.app.AlertDialog
 import android.content.Context
 import android.widget.Toast
 
 fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) =
         Toast.makeText(this, message, duration).show()
+
+fun Context.showConfirmDialog(
+        title: String, message: String,
+        positiveBtn: String = "Ok", negativeBtn: String = "Cancel",
+        actionIfAgree: () -> Unit
+) {
+    val alertDialog = AlertDialog.Builder(this).create()
+    alertDialog.setTitle(title)
+    alertDialog.setMessage(message)
+    alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, negativeBtn) { dialog, _ ->
+        dialog.dismiss()
+    }
+    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, positiveBtn) { dialog, _ ->
+        actionIfAgree()
+        dialog.dismiss()
+    }
+    alertDialog.show()
+}
