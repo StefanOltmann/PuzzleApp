2a3335af828eb31d62942d4f5699d2bb95ddda48
Juan Ramos <juancagb.17@gmail.com>
2020-06-30 19:11:47 -0500

    Refactor classes to Kotlin, Add extensions for Context

---
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/PuzzlePiece.kt
@@ -0,0 +1,21 @@
+package dragosholban.com.androidpuzzlegame
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatImageView
+
+class PuzzlePiece(context: Context?) : AppCompatImageView(context) {
+    @JvmField
+    var xCoord = 0
+
+    @JvmField
+    var yCoord = 0
+
+    @JvmField
+    var pieceWidth = 0
+
+    @JvmField
+    var pieceHeight = 0
+
+    @JvmField
+    var canMove = true
+}
---
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/util/ContextExtensions.kt
@@ -0,0 +1,7 @@
+package dragosholban.com.androidpuzzlegame.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) =
+        Toast.makeText(this, message, duration).show()
